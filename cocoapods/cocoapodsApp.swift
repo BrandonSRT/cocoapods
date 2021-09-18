0b8598938a0ec74da3e323dc2c7e0b4e725877e7
Brandon <brandon.se2009@gmail.com>
2021-09-18 12:28:00 -0400

    Initial Commit

---
+++ cocoapods/cocoapodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cocoapodsApp.swift
+//  cocoapods
+//
+//  Created by user203110 on 9/18/21.
+//
+
+import SwiftUI
+
+@main
+struct cocoapodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
